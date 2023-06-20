ee0168356cc8db8ae9da947ac7ded12847c741b8
Marquise <marquise_dawkins@aol.com>
2023-06-20 00:00:39 -0400

    test

--- js-files/home.js
+++ js-files/home.js
@@ -1,4 +1,9 @@
-// connection test
-console.log("Hello World");
-
 // for the button on hover show a pic of the lamp that it would be I save an example from amazon
+
+function adjustSize(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
