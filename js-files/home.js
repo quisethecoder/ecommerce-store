3beb6943f602e9b4b75c6767d3a2bdd2b69c0e67
Marquise <marquise_dawkins@aol.com>
2023-07-04 00:14:16 -0400

    gallery function addition

--- js-files/home.js
+++ js-files/home.js
@@ -1,2 +1,9 @@
 // for the button on hover show a pic of the lamp that it would be I save an example from amazon
 
+function photoGallery(imgs) {
+  var expandImg = document.getElementById("main-pic");
+ 
+  expandImg.src = imgs.src;
+ 
+  expandImg.parentElement.style.display = "block";
+}
