048f644c21b9fb6e311b8ac17b857c45db5fcc7a
Marquise <marquise_dawkins@aol.com>
2023-07-03 22:06:38 -0400

    function removed

--- js-files/home.js
+++ js-files/home.js
@@ -1,9 +1,2 @@
 // for the button on hover show a pic of the lamp that it would be I save an example from amazon
 
-function adjustSize(imgs) {
-    var expandImg = document.getElementById("expandedImg");
-    var imgText = document.getElementById("imgtext");
-    expandImg.src = imgs.src;
-    imgText.innerHTML = imgs.alt;
-    expandImg.parentElement.style.display = "block";
-  }
