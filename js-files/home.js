9bebe0bc886b432eac25ee3207584293d91ef379
Marquise <marquise_dawkins@aol.com>
2023-07-10 14:23:12 -0400

    fix counter bug

--- js-files/home.js
+++ js-files/home.js
@@ -19,7 +19,7 @@ let a = 1
 plus.addEventListener("click", ()=> {
   a++;
 
-  a = (a < 10) ? "0" + a : a;
+  // a = (a < 10) ? "0" + a : a;
 
   num.innerText = a;
 });
@@ -27,7 +27,8 @@ plus.addEventListener("click", ()=> {
 minus.addEventListener("click", ()=>{
   if(a > 1){
     a--;
-    a = (a<10) ? "0" + a : a;
+    // a = (a<10) ? "0" + a : a;
+
     num.innerText = a;
   }
 });
