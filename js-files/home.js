70998cab8c6a0e6ddf3d571fcbaa77398fab5d5c
Marquise <marquise_dawkins@aol.com>
2023-07-10 22:32:42 -0400

    note removal

--- js-files/home.js
+++ js-files/home.js
@@ -38,16 +38,13 @@ let a = 1
 plus.addEventListener("click", ()=> {
   a++;
 
-  // a = (a < 10) ? "0" + a : a;
-
   num.innerText = a;
 });
 
 minus.addEventListener("click", ()=>{
   if(a > 1){
     a--;
-    // a = (a<10) ? "0" + a : a;
-
+    
     num.innerText = a;
   }
 });
