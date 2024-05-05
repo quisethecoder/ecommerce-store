a8ebc1e30ca09d2230fbe1949aefccb589b6ae6c
Marquise <marquise_dawkins@aol.com>
2024-05-05 18:05:38 -0400

    footer adjus

--- js-files/home.js
+++ js-files/home.js
@@ -5,6 +5,67 @@ const minusButton = document.getElementById('minus');
 const plusButton = document.getElementById('plus');
 const addToCartButton = document.querySelector('.btn');
 
+
+
+const checkoutLink = document.querySelector(".c");
+
+
+// Photo Gallery Section start
+
+function photoGallery(imgs) {
+  var expandImg = document.getElementById("main-pic");
+ 
+  expandImg.src = imgs.src;
+ 
+  expandImg.parentElement.style.display = "block";
+}
+
+// Photo Gallery Section end
+
+
+// Color Selector Section start
+
+var btnContainer = document.getElementById("colors");
+
+var btns = btnContainer.getElementsByClassName("color-item");
+
+for (var i = 0; i < btns.length; i++){
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+// Color Selector Section end
+
+// Quantity Button Section start
+
+const plus = document.querySelector(".plus"),
+minus = document.querySelector(".minus"),
+num = document.querySelector(".num");
+
+let a = 1
+
+plus.addEventListener("click", ()=> {
+  a++;
+
+  num.innerText = a;
+});
+
+minus.addEventListener("click", ()=>{
+  if(a > 1){
+    a--;
+    
+    num.innerText = a;
+  }
+});
+
+// Quantity Button Section end
+
+// Checkout/add to cart section start
+
+
 // Initialize selected color and quantity
 let selectedColor = colorItems[0].innerText; // Default to the first color
 let quantity = parseInt(quantityDisplay.innerText);
@@ -20,7 +81,7 @@ colorItems.forEach(item => {
         item.classList.add('active');
         // Update selected color
         selectedColor = item.innerText;
-    });
+    }); 
 });
 
 // Add event listeners for quantity buttons
