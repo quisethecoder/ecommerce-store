0b388219f5c1f9e42f359a8164f5c4d4be8137df
Marquise <marquise_dawkins@aol.com>
2024-06-21 00:25:33 -0400

    hide and show paragraph

--- js-files/product.js
+++ js-files/product.js
@@ -2,6 +2,9 @@ const galleryPhotos = document.querySelectorAll(".gallery-pic");
 const colorItems = document.querySelectorAll(".color-item");
 const minusButton = document.getElementById("minus");
 const plusButton = document.querySelector(".plus");
+const infoParaOne = document.getElementById("info-paragraph-one");
+const infoParaTwo = document.getElementById("info-paragraph-two");
+const infoParaThree = document.getElementById("info-paragraph-three");
 
 let selectedColor = "Sun";
 let quantity = "1";
@@ -21,6 +24,8 @@ galleryPhotos.forEach(galleryPhoto => {
 //     });
 // });
 
+
+
 minusButton.addEventListener('click', function() {
     if (quantity > 1) {
         quantity--;
@@ -32,3 +37,58 @@ plusButton.addEventListener('click', function() {
     quantity++;
     document.querySelector('.number').textContent = quantity; 
 });
+
+
+function hide(){  
+    var hidePara = infoParaOne.style.display = "none";
+    document.getElementById("showMe").style.display = "block";
+    document.getElementById("hideMe").style.display = "none";
+ 
+ }
+ 
+ function show(){
+     var showPara= infoParaOne.style.display = "block";
+     document.getElementById("hideMe").style.display = "block";
+     document.getElementById("showMe").style.display = "none";
+ };
+
+ function hideOne(){
+    var hidePara = infoParaTwo.style.display = "none";
+    document.getElementById("showMeOne").style.display = "block";
+    document.getElementById("hideMeOne").style.display = "none";
+ };
+
+ function showOne(){
+    var showPara = infoParaTwo.style.display = "block"
+    document.getElementById("hideMeOne").style.display = "block";
+    document.getElementById("showMeOne").style.display = "none";
+ };
+
+
+ function hideTwo(){
+    var hidePara = infoParaThree.style.display = "none";
+    document.getElementById("showMeTwo").style.display = "block";
+    document.getElementById("hideMeTwo").style.display = "none";
+ };
+
+ function showTwo(){
+    var showPara = infoParaThree.style.display = "block"
+    document.getElementById("hideMeTwo").style.display = "block";
+    document.getElementById("showMeTwo").style.display = "none";
+ };
+
+
+
+//  function hideTwo(){
+//     var hidePara = infoParaThree.style.display = "none";
+
+//     document.getElementById("showMeTwo").style.display = "block";
+//     document.getElementById("hideMeTwo").style.display = "none";
+//  };
+
+//  function showTwo(){
+//     var showPara = infoParaThree.style.display = "block";
+
+//     document.getElementById("hideMeTwo").style.display = "block";
+//     document.getElementById("showMeTwo").style.diplay = "none";
+//  };
