586271200560ad3450a80e40be54c0ec37d2efa8
Marquise <marquise_dawkins@aol.com>
2024-06-20 23:28:45 -0400

    quantity button

--- js-files/product.js
+++ js-files/product.js
@@ -1,3 +1,34 @@
-// look up how to do the accordian for the how to use shipping and return policy
+const galleryPhotos = document.querySelectorAll(".gallery-pic");
+const colorItems = document.querySelectorAll(".color-item");
+const minusButton = document.getElementById("minus");
+const plusButton = document.querySelector(".plus");
 
-// if you forget what an accordian is go to bootstrap and look at the docs
+let selectedColor = "Sun";
+let quantity = "1";
+
+galleryPhotos.forEach(galleryPhoto => {
+    galleryPhoto.addEventListener('click', function(){
+        const mainPhoto = document.getElementById("main-pic");
+        mainPhoto.src = this.src;
+    });
+});
+
+// colorItems.forEach(item => {
+//     item.addEventListener('click', function(){
+//         colorItems.forEach(i => i.classList.remove('.active'));
+//         this.classList.add('active');
+//         selectedColor = this.innerText;
+//     });
+// });
+
+minusButton.addEventListener('click', function() {
+    if (quantity > 1) {
+        quantity--;
+        document.querySelector('.number').textContent = quantity;
+    }
+});
+
+plusButton.addEventListener('click', function() {
+    quantity++;
+    document.querySelector('.number').textContent = quantity; 
+});
