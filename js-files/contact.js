54cb2d6c30314b544624be9554b3b7d76f457c85
Marquise <marquise_dawkins@aol.com>
2024-08-04 21:46:01 -0400

    add to cart badge

--- js-files/contact.js
+++ js-files/contact.js
@@ -13,3 +13,21 @@ const handleContact = () => {
 
 
 trackingButton.addEventListener("click", handleContact);
+
+
+
+    let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
+
+    function updateCartBadge() {
+        const cartBadge = document.querySelector('.cart-badge');
+        const totalItems = cart.reduce((acc, item) => acc + item.quantity, 0); // Calculate total items
+        cartBadge.textContent = totalItems;
+        cartBadge.style.display = totalItems > 0 ? 'block' : 'none'; // Display badge if there are items
+    }
+    
+    updateCartBadge();
+    
+
+
+
+
